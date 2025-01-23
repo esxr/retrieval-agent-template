bc08b78a6646f2e8cb470dbc0706e01b334dfa25
atharva <atharvagupta2003@gmail.com>
2025-01-23 22:09:08 +0530

    initialised custom ingestion graph

--- src/retrieval_graph/__init__.py
+++ src/retrieval_graph/__init__.py
@@ -30,5 +30,6 @@ and individual component documentation within the retrieval_graph package.
 from retrieval_graph.graph import graph
 from retrieval_graph.index_graph import graph as index_graph
 from retrieval_graph.jira_index_graph import graph as jira_index_graph
+from retrieval_graph.custom_docs_graph import graph as custom_index_graph
 
 __all__ = ["graph", "index_graph", "jira_index_graph"]
